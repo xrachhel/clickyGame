5a37892263bcc811aec64a94e99f532b50029930
Rachel Yeung <rachel.cmyeung@gmail.com>
2020-02-07 09:51:34 -0800

    Updates

---
+++ precache-manifest.1abf201e0682df756a5ec3d5e1c63d9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f57be3e96b6b1073cd56fa1242d6f44",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "4de666560868a54be0b4",
+    "url": "/clickyGame/static/css/main.56ea13e9.chunk.css"
+  },
+  {
+    "revision": "bc5e42eb7f5dd25e7f41",
+    "url": "/clickyGame/static/js/2.065b170a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clickyGame/static/js/2.065b170a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de666560868a54be0b4",
+    "url": "/clickyGame/static/js/main.b9a82acc.chunk.js"
+  },
+  {
+    "revision": "1948471f16c44a1d32e0",
+    "url": "/clickyGame/static/js/runtime-main.a3181178.js"
+  }
+]);
